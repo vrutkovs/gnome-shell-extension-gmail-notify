a62d248051942580cb94eb6909a3b3fa34a80ac3
Vadim Rutkovsky <roignac@gmail.com>
2012-07-01 12:20:18 +0300

    Created proper settings schema and configuration, fixes #1

--- prefs.js
+++ prefs.js
@@ -15,51 +15,64 @@
 **/
 
 const Gtk = imports.gi.Gtk;
+const GObject = imports.gi.GObject;
 
-const Gettext = imports.gettext.domain('gmail_notify');
+const Gettext = imports.gettext.domain('gnome-shell-extensions-gmail-notify');
 const _ = Gettext.gettext;
 
 const Me = imports.misc.extensionUtils.getCurrentExtension();
 const Lib = Me.imports.lib;
+const Convenience = Me.imports.convenience;
 
-let settings;
+let settings_choice;
+let settings_string;
 let settings_bool;
 let settings_range;
 
 function init() {
-    Lib.initTranslations(Me);
-    settings = Lib.getSettings(Me);
+    this._settings = Convenience.getSettings();
+    settings_choice = {
+        "position": { label: _("Button position"),
+                      options: {"Left"  : 2,
+                                "Center": 1,
+                                "Right" : 0},
+                  }
+    }
+    settings_string = {
+        "number-format": { label: _("Text format"),
+                           help: _("Available replacements:"
+                                 + "{unread} - number of unread messages"
+                                 + "{total} - total number of messages in inbox"),
+                           default: "{unread} ({total})"}
+    }
     settings_bool = {
-        notifyOnNewMessages: {label: _("Notify about incoming mail")},
-        rundefault: {label: _("Allow to start the default media player")},
-        volume: {label: _("Show the media player volume slider")},
-        position: {label: _("Show the media player position slider")},
-        playlists: {label: _("Show media player playlists")}
+        "notify-on-new-messages" : {label: _("Notify about incoming mail")},
+        "use-default-mail-reader": {label: _("Use default mail reader instead of gmail in browser")},
+        "show-unread-numbers"    : {label: _("Show number of unread messages")},
     };
     settings_range = {
-        timeout: {label: _("Check interval"),
+        "timeout": {label: _("Check interval"),
                   help: _("Amount of time to check for new messages in seconds"),
                   min: 60, max: 360, step: 30, default: 180}
     };
 }
 
 function createRangeSetting(setting) {
-
     let hbox = new Gtk.Box({ orientation: Gtk.Orientation.HORIZONTAL });
 
     let setting_label = new Gtk.Label({ label: settings_range[setting].label,
                                         xalign: 0 });
 
-    let setting_range = Gtk.HScale.new_with_range(settings_range[setting].min,
-                                                  settings_range[setting].max,
-                                                  settings_range[setting].step);
-    setting_range.set_value(settings.get_int(setting));
+    let setting_range = Gtk.HScale.new_with_range( settings_range[setting].min,
+                                                   settings_range[setting].max,
+                                                   settings_range[setting].step);
+    setting_range.set_value(this._settings.get_int(setting));
     setting_range.set_draw_value(false);
     setting_range.add_mark(settings_range[setting].default,
                            Gtk.PositionType.BOTTOM, null);
     setting_range.set_size_request(200, -1);
     setting_range.connect('value-changed', function(slider) {
-        settings.set_int(setting, slider.get_value());
+        this._settings.set_int(setting, slider.get_value());
     });
 
     if (settings_range[setting].help) {
@@ -80,9 +93,9 @@ function createBoolSetting(setting) {
     let setting_label = new Gtk.Label({label: settings_bool[setting].label,
                                        xalign: 0 });
 
-    let setting_switch = new Gtk.Switch({active: settings.get_boolean(setting)});
+    let setting_switch = new Gtk.Switch({active: this._settings.get_boolean(setting)});
     setting_switch.connect('notify::active', function(button) {
-        settings.set_boolean(setting, button.active);
+        _settings.set_boolean(setting, button.active);
     });
 
     if (settings_bool[setting].help) {
@@ -96,6 +109,72 @@ function createBoolSetting(setting) {
     return hbox;
 }
 
+function createStringSetting(setting) {
+
+    let hbox = new Gtk.Box({ orientation: Gtk.Orientation.HORIZONTAL });
+
+    let setting_label = new Gtk.Label({label: settings_string[setting].label,
+                                       xalign: 0 });
+
+    let setting_entry = new Gtk.Entry({text: this._settings.get_string(setting)});
+    setting_entry.connect('notify::leave', function(entry) {
+        this._settings.set_string(setting, entry.text);
+    });
+
+    if (settings_string[setting].help) {
+        setting_label.set_tooltip_text(settings_string[setting].help)
+        setting_entry.set_tooltip_text(settings_string[setting].help)
+    }
+
+    hbox.pack_start(setting_label, true, true, 0);
+    hbox.add(setting_entry);
+
+    return hbox;
+}
+
+function createChoiceSetting(setting) {
+
+    this._model = new Gtk.ListStore();
+    this._model.set_column_types([GObject.TYPE_STRING, GObject.TYPE_INT]);
+    for (option in settings_choice[setting].options) {
+        let value = settings_choice[setting].options[option];
+        this._model.insert_with_valuesv(-1, [ 0, 1 ], [ option, value ]);
+    }
+    
+
+    let hbox = new Gtk.Box({ orientation: Gtk.Orientation.HORIZONTAL });
+
+    let setting_label = new Gtk.Label({label: settings_choice[setting].label,
+                                       xalign: 0 });
+
+    let setting_choice = new Gtk.ComboBox({ margin_left: 8,
+                                            hexpand: true });
+    setting_choice.set_model(this._model);
+    setting_choice.set_active(this._settings.get_int(setting));
+    let renderer = new Gtk.CellRendererText();
+    setting_choice.pack_start(renderer, true);
+    setting_choice.add_attribute(renderer, 'text', 0);
+
+    setting_choice.connect('changed', function(combobox) {
+        let [success, iter] = setting_choice.get_active_iter();
+        if (!success)
+            return;
+
+        _settings.set_int(setting, _model.get_value(iter, 1));
+    });
+
+    if (settings_choice[setting].help) {
+        setting_label.set_tooltip_text(settings_choice[setting].help)
+        setting_choice.set_tooltip_text(settings_choice[setting].help)
+    }
+
+    hbox.pack_start(setting_label, true, true, 0);
+    hbox.add(setting_choice);
+
+    return hbox;
+    
+}
+
 function buildPrefsWidget() {
     let frame = new Gtk.Box({ orientation: Gtk.Orientation.VERTICAL,
                               border_width: 10 });
@@ -106,6 +185,17 @@ function buildPrefsWidget() {
         let hbox = createBoolSetting(setting);
         vbox.add(hbox);
     }
+
+    for (setting in settings_string) {
+        let hbox = createStringSetting(setting);
+        vbox.add(hbox);
+    }
+
+    for (setting in settings_choice) {
+        let hbox = createChoiceSetting(setting);
+        vbox.add(hbox);
+    }
+
 
     for (setting in settings_range) {
         let hbox = createRangeSetting(setting);
